bc76323852229de18e52eb7b8e81078b1e520c15
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-06-11 16:20:16 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 export const App = () => {
+  state = {
+    contacts: [],
+    name: 'fsdfasdfsdf'
+  }
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <input
+        type="text"
+        name="{this.state.name}"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
     </div>
   );
 };
